1857530eaf6fcecbdbd424c595ee34645fedc91e
Eric Range <mail@eric-range.de>
2015-06-23 13:46:27 +0200

    Update

--- chart.js
+++ chart.js
@@ -1,6 +1,6 @@
 function Chart() {
 
-  var w = 600,
+  var w = 1000,
     h = 400,
     data = null,
     path = d3
@@ -8,27 +8,36 @@ function Chart() {
     .append("svg")
     .attr("width", w + "px")
     .attr("height", h + "px")
-		.attr("style", "overflow: visible");
+    .attr("style", "overflow: visible");
 
-	data = [];
+  data = [];
 
-	for (var i = -100; i <= 100; i++) {
-		data.push({
-			x: i / 10,
-			y: i / 10
-		});
-	}
+  for (var i = -50; i <= 50; i+=0.1) {
+    data.push({
+      x: i,
+      y: Math.pow(i, 2)
+    });
+  }
+
+	console.log(data);
+
+  var maxX = _.max(data, function(x) {
+    return x.x;
+  });
+  var maxY = _.max(data, function(y) {
+    return y.y;
+  });
 
   var x = d3.time.scale()
-    .domain([-100, 100])
+    .domain([-maxX.x, maxX.x])
     .range([0, w]);
 
   var y = d3.scale.linear()
     .domain([-100, 100])
+    //.domain([-5, 5])
     .range([h, 0]);
 
   var line = d3.svg.line()
-    .interpolate("bundle")
     .x(function(d) {
       return x(d.x);
     })
@@ -43,8 +52,8 @@ function Chart() {
     .transition()
     .duration(450)
     .attr("d", line)
-		.attr("stroke", "red")
-		.attr("fill", "none");
+    .attr("stroke", "red")
+    .attr("fill", "none");
 
   var yAxis = d3.svg.axis()
     .scale(y)
@@ -52,21 +61,21 @@ function Chart() {
 
   var xAxis = d3.svg.axis()
     .scale(x)
-		.tickFormat(d3.format("d"))
+    .tickFormat(d3.format("d"))
     .orient("bottom");
 
   path.append("g")
     .attr("class", "y axis")
-		.attr("transform", "translate(" + (w / 2) + ", 0)")
-		.attr("fill", "none")
-		.attr("stroke", "#999")
+    .attr("transform", "translate(" + (w / 2) + ", 0)")
+    .attr("fill", "none")
+    .attr("stroke", "#999")
     .call(yAxis);
 
   path.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0, " + (h / 2) + ")")
-		.attr("fill", "none")
-		.attr("stroke", "#999")
+    .attr("fill", "none")
+    .attr("stroke", "#999")
     .call(xAxis);
 
   function drawDistribution(Distribution) {
